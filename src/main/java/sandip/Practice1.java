492fce0db201c7ffab7f31df4da8a173aa801ad5
User <User@DESKTOP-T3V7MRI>
2022-08-26 14:25:37 +0530

    1st commit

---
+++ src/main/java/sandip/Practice1.java
@@ -0,0 +1,14 @@
+package sandip;
+
+import javax.management.openmbean.OpenMBeanAttributeInfo;
+
+public class Practice1 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		
+		System.out.println(a+b);
+	}
+}
